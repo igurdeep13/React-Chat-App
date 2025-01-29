9039984661ff1f92158ed20d34fe19e6819abb58
Gurdeep Singh <g.s.basantpuria@gmail.com>
2025-01-28 19:48:10 -0500

    Add message and css classes

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -2,8 +2,7 @@ import { Client, Databases } from "appwrite";
 
 export const PROJECT_ID = "";
 export const DATABASE_ID = "";
-
-const client = new Client();
+export const client = new Client();
 
 client.setProject("6798748d00242c8d927e");
 
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { databases } from "../appwriteConfig";
 
 const Room = () => {
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
     getMessages();
   }, []);
 
-  const getMessages = async () => {};
+  const databaseID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+  const collectionID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+  //   console.log(databaseID, collectionID);
 
-  return <div>Room</div>;
+  const getMessages = async () => {
+    const response = await databases.listDocuments(databaseID, collectionID);
+    console.log(response);
+    setMessages(response.documents);
+  };
+
+  return (
+    <main className="container">
+      <div className="room--container">
+        <div>
+          {messages.map((messages) => (
+            <div key={messages.$id} className="message--wrapper">
+              <div className="message--header">
+                <small className="message-timestamp">
+                  {messages.$createdAt}
+                </small>
+              </div>
+
+              <div className="message--body">
+                <span>{messages.body}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Room;
