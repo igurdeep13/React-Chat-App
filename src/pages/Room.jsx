a8e493a13505b61717579ace6f17d84ae7cadc8d
Gurdeep Singh <g.s.basantpuria@gmail.com>
2025-01-29 02:37:32 -0500

    Add form and other logic

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { databases } from "../appwriteConfig";
+import { ID } from "appwrite";
 
 const Room = () => {
   const [messages, setMessages] = useState([]);
+  const [messageBody, setMessageBody] = useState("");
 
   useEffect(() => {
     getMessages();
@@ -11,6 +13,25 @@ const Room = () => {
   const databaseID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
   const collectionID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
   //   console.log(databaseID, collectionID);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let payload = {
+      body: messageBody,
+    };
+
+    let response = await databases.createDocument(
+      databaseID,
+      collectionID,
+      ID.unique(),
+      payload
+    );
+
+    console.log("created!", response);
+    setMessages((prev) => [response, ...messages]);
+    setMessageBody("");
+  };
 
   const getMessages = async () => {
     const response = await databases.listDocuments(databaseID, collectionID);
@@ -21,6 +42,21 @@ const Room = () => {
   return (
     <main className="container">
       <div className="room--container">
+        <form onSubmit={handleSubmit} id="message--form">
+          <div>
+            <textarea
+              required
+              maxLength="1000"
+              placeholder="Say Something..."
+              onChange={(e) => setMessageBody(e.target.value)}
+              value={messageBody}
+            ></textarea>
+          </div>
+
+          <div className="send-btn--wrapper">
+            <input className="btn btn--secondary" type="submit" value="Send" />
+          </div>
+        </form>
         <div>
           {messages.map((messages) => (
             <div key={messages.$id} className="message--wrapper">
